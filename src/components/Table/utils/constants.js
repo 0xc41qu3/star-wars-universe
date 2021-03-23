1c4ba2a6c8b258bef7200fc8f4920210376a0d59
Caique Oliveira <caique_m_oliveira@icloud.com>
2021-03-22 23:41:10 -0300

    chore: create sort and comparison operators contants

---
+++ src/components/Table/utils/constants.js
@@ -0,0 +1,16 @@
+/**
+ * Available comparison operators for filtering tabular data.
+ */
+export const COMPARISON_OPERATOR = {
+  EQUAL: "0",
+  GREATER_THAN: "1",
+  LESS_THAN: "-1",
+};
+
+/**
+ * Available options for sorting tabular data.
+ */
+export const SORT = {
+  ASC: "asc",
+  DESC: "desc",
+};
